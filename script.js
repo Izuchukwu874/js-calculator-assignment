5420931488cbbbfb64d650b1000f9cb50b1c4d41
Izuchukwu874 <izuchukwuohaja@gmail.com>
2025-07-22 10:18:12 +0100

    Task 2: JS variables and basic functions

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
- 
+ // Variables
+let currentInput = '0';
+let firstOperand = null;
+let operator = null;
+let waitingForSecondOperand = false;
+
+const calculationHistory = [];
+
+// Reset calculator
+function clearCalculator() {
+  currentInput = '0';
+  firstOperand = null;
+  operator = null;
+  waitingForSecondOperand = false;
+}
+
+// Append a number
+const appendNumber = function (number) {
+  if (waitingForSecondOperand) {
+    currentInput = String(number);
+    waitingForSecondOperand = false;
+  } else {
+    currentInput = currentInput === '0' ? String(number) : currentInput + number;
+  }
+}
+
+// Update the display
+const updateDisplay = () => {
+  document.getElementById('display').value = currentInput;
+}
+
